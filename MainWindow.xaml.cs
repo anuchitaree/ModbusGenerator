7ff16195e7cba71e62123d48da97d134c26e7e25
diatanuchit <anuchit.aree.a9f@ap.denso.com>
2024-10-22 09:58:12 +0700

    change to Uint

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -18,8 +18,8 @@ namespace ModbusGenerator
         DispatcherTimer timerPollWrite = new DispatcherTimer();
         DispatcherTimer timerPollRead = new DispatcherTimer();
 
-        int powerEnergy = 0;
-        int prodCapture = 0;
+        UInt32 powerEnergy = 0;
+        UInt32 prodCapture = 0;
 
         public ChartValues<double> Values1 { get; set; }
         public ChartValues<double> Values2 { get; set; }
@@ -48,10 +48,10 @@ namespace ModbusGenerator
             modbusClient.Port = 502;
             modbusClient.Connect();
 
-            timerPollRead.Interval = TimeSpan.FromMilliseconds(5000);
+            timerPollRead.Interval = TimeSpan.FromMilliseconds(1000);
             timerPollRead.Tick += TimerPollRead_Tick;
 
-            timerPollWrite.Interval = TimeSpan.FromMilliseconds(1);
+            timerPollWrite.Interval = TimeSpan.FromMilliseconds(1000);
             timerPollWrite.Tick += TimerPollWrite_Tick;
 
             timerPollWrite.Start();
@@ -64,32 +64,31 @@ namespace ModbusGenerator
             {
                 if (modbusClient.Connected == true)
                 {
+
                     int[] vals = modbusClient.ReadHoldingRegisters(0, 4);
-                    //Console.WriteLine($"vals[0]: {vals[0]} ,vals[1]:{vals[1]}");
 
-                    int powerEnergy0 = vals[0] << 16;
-                    int powerEnergy1 = vals[1];
-
-                    int prodCapture0 = vals[2] << 16;
-                    int prodCapture1 = vals[3];
-
-                    int powerEnergy = powerEnergy0 | powerEnergy1;
-                    int prodCapture = prodCapture0 | prodCapture1;
+                    UInt32 powerEnergy0 = (UInt32)vals[0] << 16;
+                    UInt32 powerEnergy1 = (UInt32)vals[1];
+                    UInt32 powerEnergy = powerEnergy0 | (powerEnergy1 & 0x0000FFFF); //2147483647
 
 
-                 
 
-                    string hexValue1 = vals[0].ToString("X");  // 0000 7FFF
-                    string hexValue2 = vals[1].ToString("X"); // FFFF FFFF
+                    UInt32 prodCapture0 = (UInt32)vals[2] << 16;
+                    UInt32 prodCapture1 = (UInt32)vals[3];
+                    UInt32 prodCapture = prodCapture0 | (prodCapture1 & 0x0000FFFF);
+
+
+                    //string hexValue1 = vals[0].ToString("X");  // 0000 7FFF
+                    //string hexValue2 = vals[1].ToString("X"); // FFFF FFFF
 
 
                     //gauge1.Value = powerEnergy;
-                    lbPowerEnergy.Content = $"Power energy (kw-h) : {powerEnergy.ToString()} [int32]";
-                    lbProdCapture.Content = $"Production capture  : {prodCapture.ToString()} [int32]";
+                    lbPowerEnergy.Content = $"Power energy (kw-h) : {powerEnergy:n0} [UInt32]";
+                    lbProdCapture.Content = $"Production capture  : {prodCapture:n0} [UInt32]";
 
 
-                    chart1.Values.Add(powerEnergy);
-                    chart2.Values.Add(prodCapture);
+                    chart1.Values.Add((int)powerEnergy);
+                    chart2.Values.Add((int)prodCapture);
 
 
                     var len = chart1.Values.Count;
@@ -122,18 +121,31 @@ namespace ModbusGenerator
             //ModbusServer.HoldingRegisters regs = modbusServer.holdingRegisters;
             //regs[2] = ival;
 
+            if (powerEnergy > 999999999)
+            {
+                powerEnergy = 0;
+            }
+
+            if (prodCapture > 999999999)
+            {
+                prodCapture = 0;
+            }
+
             Random rnd = new Random();
             int energy = rnd.Next(1, 3);
-            powerEnergy = powerEnergy + energy;
-            powerEnergy = 2147483647;  // 7FFF FFFF
+            UInt32 randomEnergy = Convert.ToUInt32(energy);
+
+            powerEnergy = powerEnergy + randomEnergy;
+
+            //powerEnergy = 2147483647;  // 7FFF FFFF
 
             byte[] bytes = BitConverter.GetBytes(powerEnergy);
             short firstHalf = BitConverter.ToInt16(bytes, 0);
             short secondHalf = BitConverter.ToInt16(bytes, 2);
 
 
-            string hexValue1 = firstHalf.ToString("X");
-            string hexValue2 = secondHalf.ToString("X");
+            //string hexValue1 = firstHalf.ToString("X");
+            //string hexValue2 = secondHalf.ToString("X");
 
 
 
@@ -143,7 +155,7 @@ namespace ModbusGenerator
             short secondHalf1 = BitConverter.ToInt16(bytes1, 2);
 
 
-            short ival = short.Parse("15");
+            //short ival = short.Parse("15");
             ModbusServer.HoldingRegisters regs = modbusServer.holdingRegisters;
             regs[2] = firstHalf;  /// FFFF
             regs[1] = secondHalf;  /// 7FFF
@@ -151,25 +163,12 @@ namespace ModbusGenerator
             regs[4] = firstHalf1;
             regs[3] = secondHalf1;
 
-            //regs[2] = 32767; // firstHalf;
-            //regs[1] = 90; //secondHalf;
-
-            //regs[4] = 32767; // firstHalf1;
-            //regs[3] = 120; //secondHalf1;
-
-
-
-
-
-
-
         }
 
         private void Window_Closed(object sender, EventArgs e)
         {
             timerPollWrite.Stop();
             timerPollRead.Stop();
-
         }
     }
 }
